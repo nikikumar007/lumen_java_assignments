0b57415eee95ad94d4c533dfad7e3230ad778e94
Neema <Neema@LAPTOP-6L29FC6K>
2020-12-07 17:18:21 +0530

    update

--- assignment2/src/com/example/demo/Book.java
+++ assignment2/src/com/example/demo/Book.java
@@ -1,61 +1,52 @@
 package com.example.demo;
 
 public class Book {
-private int id;
-private String bookName;
-private String author;
-private double price;
+	private int id;
+	private String bookName;
+	private String author;
+	private double price;
 
-public Book() {
-}
+	public Book() {
+	}
 
-public Book(int id, String bookName, String author, double price) {
-	super();
-	this.id = id;
-	this.bookName = bookName;
-	this.author = author;
-	this.price = price;
-}
+	public Book(int id, String bookName, String author, double price) {
+		super();
+		this.id = id;
+		this.bookName = bookName;
+		this.author = author;
+		this.price = price;
+	}
 
+	public int getId() {
+		return id;
+	}
 
-public int getId() {
-	return id;
-}
+	public void setId(int id) {
+		this.id = id;
+	}
 
+	public String getBookName() {
+		return bookName;
+	}
 
-public void setId(int id) {
-	this.id = id;
-}
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
 
+	public String getAuthor() {
+		return author;
+	}
 
-public String getBookName() {
-	return bookName;
-}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
 
+	public double getPrice() {
+		return price;
+	}
 
-public void setBookName(String bookName) {
-	this.bookName = bookName;
-}
-
-
-public String getAuthor() {
-	return author;
-}
-
-
-public void setAuthor(String author) {
-	this.author = author;
-}
-
-
-public double getPrice() {
-	return price;
-}
-
-
-public void setPrice(double price) {
-	this.price = price;
-}
-
+	public void setPrice(double price) {
+		this.price = price;
+	}
 
 }
--- assignment2/src/com/example/demo/apps/Application.java
+++ assignment2/src/com/example/demo/apps/Application.java
@@ -6,19 +6,20 @@ import com.example.demo.Book;
 import com.example.demo.service.BookService;
 
 public class Application {
-	public static void main(String[] args)
-	{
-	Book b1=new Book(1,"Nights Tale","Nik",5000);
-	System.out.println(b1.getPrice());
-	BookService bs=new BookService();
-	double dis=bs.calculateDiscount(b1);
-System.out.println("Discount="+ dis);
-	System.out.println("Total Price"+(b1.getPrice()-dis));
-	Scanner sc =new Scanner(System.in);
-	System.out.println("Enter Customer Type");
-	String custType=sc.next();
-	double dis1=bs.calculateDiscount(b1, custType);
-	System.out.println("Discount="+dis1);
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter book id");
+		int id = sc.nextInt();
+		Book b1 = new Book(id, "Nights Tale", "Nik", 5000);
+		System.out.println(b1.getPrice());
+		BookService bs = new BookService();
+		double dis = bs.calculateDiscount(b1);
+		System.out.println("Discount=" + dis);
+		System.out.println("Total Price" + (b1.getPrice() - dis));
+		System.out.println("Enter Customer Type");
+		String custType = sc.next();
+		double dis1 = bs.calculateDiscount(b1, custType);
+		System.out.println("Discount=" + dis1);
+		sc.close();
 	}
 }
-	
