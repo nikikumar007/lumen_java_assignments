daf31001295d417a7daf5932b22c2c138a6b3825
Neema <Neema@LAPTOP-6L29FC6K>
2020-12-07 12:55:16 +0530

    updated

--- assignment1/src/com/example/demo/Application.java
+++ assignment1/src/com/example/demo/Application.java
@@ -6,8 +6,11 @@ public class Application {
 		System.out.println(bill1.getName());
 		System.out.println(bill1.getPhoneNumber());
 		System.out.println(bill1.getPlanName());
-		System.out.println(bill1.telecomName);
+		System.out.println(mobileBill.telecomName);
 		mobileBill bill2= new mobileBill("sam",9916040364l,"90days");
-		
+		System.out.println(bill2.getName());
+		System.out.println(bill2.getPhoneNumber());
+		System.out.println(bill2.getPlanName());
+		System.out.println(mobileBill.telecomName);
 	}
 }
