32f6cce33aa999c81cf5ecc8bffcd85da0e28360
Neema <Neema@LAPTOP-6L29FC6K>
2020-12-08 15:26:34 +0530

    update

--- abstract/src/com/example/demo/Application.java
+++ abstract/src/com/example/demo/Application.java
@@ -1,22 +1,24 @@
 package com.example.demo;
 
-import java.util.Scanner;
-
 import com.example.demo.model.BankAccount;
+import com.example.demo.model.BuisnessAccount;
 import com.example.demo.model.SavingsAccount;
 
 public class Application {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		//supertype=subtype
-BankAccount account = new SavingsAccount(983, "Nik", 1500, "Raj");
-account.deposit(1500);
-account.deposit(17000);
-account.deposit(1000000);
-account.withdraw(16000);
-System.out.println("Balance:"+account.getCurrentBalance());
 
-}
+		// supertype=subtype
+		BankAccount account = new SavingsAccount(983, "Nik", 1500, "Raj");
+		account.deposit(1500);
+		account.deposit(17000);
+		account.deposit(1000000);
+		account.withdraw(16000);
+		BankAccount account1 = new BuisnessAccount(9883, "Nik", 15000, "Sanj");
+		account1.deposit(1500);
+		account1.deposit(17000);
+		System.out.println("Balance:" + account1.getCurrentBalance());
+
+	}
 }
